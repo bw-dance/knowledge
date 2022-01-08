d938908ac41c06e385a6bbbfe3d7947a0455e64a
zhq-res <1427421650@qq.com>
2022-01-08 21:06:10 +0800

    save message

--- mystudy/AllCode/JavaWebCode/aishangboke/out/artifacts/aishangboke_war_exploded/js/changeblog.js
+++
@@ -1,8 +0,0 @@
-layui.use('form', function() {
-    var form = layui.form;
-    //监听提交
-    form.on('submit(formDemo)', function(data) {
-        layer.msg(JSON.stringify(data.field));
-        return false;
-    });
-});
