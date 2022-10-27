7ba33515a581d6acd7061ea71406b994994e7e15
zhq <1427421650@qq.com>
2022-10-27 08:48:23 +0800

    save note

---
+++ submit.bat
@@ -0,0 +1,18 @@
+@echo off
+echo "DOCS PUSH BAT"
+
+echo "1. Move to working directory" 
+cd G:\ComputerStudy\cloudnote\knowledge\knowledges
+
+echo "2. Start submitting code to the local repository"
+git add .
+
+echo "3. Commit the changes to the local repository"
+git commit -m "save note"
+
+echo "4. Push the changes to the remote git server"
+git push gitee HEAD:develop
+git push github HEAD:develop
+ 
+echo "Batch execution complete!"
+pause
