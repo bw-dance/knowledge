a0a39652a4177195fdeb7588d1eafc01117c9949
zhq <1427421650@qq.com>
2022-11-03 22:30:50 +0800

    save note

--- mystudy/AllCode/JavaWebCode/data_algorithms/data_structures/src/JavaCollection/map/StudyHashMap.java
+++ mystudy/AllCode/JavaWebCode/data_algorithms/data_structures/src/JavaCollection/map/StudyHashMap.java
@@ -1,5 +1,7 @@
 package JavaCollection.map;
 
+import java.lang.reflect.Field;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
@@ -8,7 +10,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class StudyHashMap {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws NoSuchFieldException, IllegalAccessException {
 //        Map<String,String> map = new HashMap<>();
 //        map.put("1","张三");
 //        map.put("1","李四");
@@ -22,14 +24,49 @@ public class StudyHashMap {
 //        String intern = a.intern();
 //        System.out.println(intern=="12");
 //        System.out.println(a=="12");
-        String s  = new String("a")+new String("b");
-        System.out.println(s=="ab");//false s引用的是堆中的new String("ab"),ab编译后能够确定，运行的时候已放入常量池
-        String s2 = s.intern();//此时常量池已有ab，s2指向字符串常量池ab
-        System.out.println(s=="ab");//false
-        System.out.println(s2=="ab");//true
+//        String s  = new String("a")+new String("b");
+//        System.out.println(s=="ab");//false s引用的是堆中的new String("ab"),ab编译后能够确定，运行的时候已放入常量池
+//        String s2 = s.intern();//此时常量池已有ab，s2指向字符串常量池ab
+//        System.out.println(s=="ab");//false
+//        System.out.println(s2=="ab");//true
 
-        ExecutorService executorService = Executors.newFixedThreadPool(10);
-        executorService.submit()
+//        ExecutorService executorService = Executors.newFixedThreadPool(10);
+//        executorService.submit()
+
+
+//        final char[] a = {'1', '1', '1'};
+//        a[0] = '0';
+//        a[2] = '0';
+//        System.out.println(Arrays.toString(a));
+//        People p = new People();
+//        System.err.println(p.str.hashCode());
+//        Field field = People.class.getDeclaredField("str");
+//        field.setAccessible(true);
+//        field.set(p, "12345");
+//        System.err.println(p.str.hashCode());
+
+
+//        String str = new String("abc");
+        String str ="abc";
+        System.out.println(str+"的hashcode=	"+str.hashCode());
+        System.out.println(str);
+        Field f = str.getClass().getDeclaredField("value");
+        f.setAccessible(true);
+        char[] b=(char[]) f.get(str);
+        b[0]='1';
+        System.out.println(str+"的hashcode=	"+str.hashCode());
+        System.out.println(str);
+
+
+
+//        System.err.println(p.str);// 12345 修改String值成功
+//        System.out.println(p.str.hashCode());
+//        p.str="123123123";
+//        System.out.println(p.str.hashCode());
 
     }
+}
+
+class People {
+    String str = "123";
 }
