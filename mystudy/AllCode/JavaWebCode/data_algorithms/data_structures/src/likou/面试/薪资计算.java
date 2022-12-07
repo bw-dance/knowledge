3fc466386ff2ebba86529b26323e0e46dd2895b1
zhq <1427421650@qq.com>
2022-12-07 22:30:45 +0800

    save note

--- mystudy/AllCode/JavaWebCode/data_algorithms/data_structures/src/likou/面试/薪资计算.java
+++ mystudy/AllCode/JavaWebCode/data_algorithms/data_structures/src/likou/面试/薪资计算.java
@@ -1,6 +1,5 @@
 package likou.面试;
 
-import java.util.Scanner;
 
 public class 薪资计算 {
     public static void main(String[] args) {
@@ -12,27 +11,50 @@ public class 薪资计算 {
                 //公积金
                 double accumulationFund = 0.12;
                 //每月房租，燃气，电费
-                int rent = 3100;
+                int rent = 2000;
                 //每月自己生活费
-                int livingExpenses = 2500;
-                computedWages(i, month, accumulationFund, livingExpenses, rent);
+                int livingExpenses = 2000;
+                //已交的金额
+                int  hasSub = 48000;
+                //工资计算
+                computedWages(i, month, accumulationFund, livingExpenses, rent, hasSub);
             }
         }
     }
 
-    private static double computedWages(int allSalary, int month, double accumulationFund, int livingExpenses, int rent) {
+    private static double computedWages(int allSalary, int month, double accumulationFund, int livingExpenses, int rent, int hasSub) {
         int monthSalary = allSalary / month;
-        double last = allSalary - insuranceAndFoud(accumulationFund, monthSalary)  ;//- LifeAndRent(livingExpenses, rent)- back(allSalary)
-        System.out.println(allSalary + ":" + (int) last);
+        double insuranceAndFoud = insuranceAndFoud(accumulationFund, monthSalary);
+        double tax = tax(allSalary - insuranceAndFoud - (monthSalary * (month - 12)));
+        double lifeAndRent = LifeAndRent(livingExpenses, rent);
+        double back = back(allSalary, hasSub);
+        double last = allSalary - insuranceAndFoud - tax - lifeAndRent - back;
+        System.out.println("------------" + allSalary + "万-----------------");
+        System.out.println("还钱：" + back);
+        System.out.println("最后剩余：" + last);
         return last;
     }
 
-    private static double back(int allSalary) {
+    /**
+     * 回款
+     *
+     * @param allSalary
+     * @param hasSub
+     * @return
+     */
+    private static double back(int allSalary, int hasSub) {
         int backSalary = allSalary - 180000;
-        double sumBack = 48000 + (backSalary > 0 ? backSalary * 0.6 : 0);
-        return sumBack<150000?sumBack:150000;
+        double sumBack = 48000+ (backSalary > 0 ? backSalary * 0.6 : 0);
+        return sumBack < 150000 ? sumBack-hasSub : 150000-hasSub;
     }
 
+    /**
+     * 公积金
+     *
+     * @param accumulationFund
+     * @param monthSalary
+     * @return
+     */
     private static double insuranceAndFoud(double accumulationFund, int monthSalary) {
         double unemploymentInsurance = 0.02;
         double endowmentInsurance = 0.08;
@@ -42,13 +64,37 @@ public class 薪资计算 {
     }
 
 
+    /**
+     * 生活成本和房租
+     *
+     * @param livingExpenses
+     * @param rent
+     * @return
+     */
     private static double LifeAndRent(int livingExpenses, int rent) {
         return (livingExpenses + rent) * 12;
     }
 
-//    private static double tax(int allSalary) {
-//
-//
-//        return (livingExpenses + rent) * 12;
-//    }
+    /**
+     * 税务（工资-五险一金后-60000的部分算税，实际中还会扣除，这是按交最多税算的，税的话拿400000最高产生15,246税）
+     * 另外 年薪40万，减完五险一金和60000的部分算税在25万左右，因此算的是0-300000之前的税收，之后的没有计算
+     *
+     * @param taxSalary
+     * @return
+     */
+    private static double tax(double taxSalary) {
+        double tax = 0;
+        taxSalary -= 60000;
+        if (taxSalary > 0 && taxSalary <= 36000) {
+            tax += taxSalary * 0.03;
+            return tax;
+        } else if (taxSalary > 36000 && taxSalary <= 144000) {
+            tax += 36000 * 0.03 + (taxSalary - 36000) * 0.1 - 2520;
+            return tax;
+        } else if (taxSalary > 144000 && taxSalary <= 300000) {
+            tax += 36000 * 0.03 + (144000 - 36000) * 0.1 - 2520 + (taxSalary - 144000) * 0.2 - 16920;
+            return tax;
+        }
+        return tax;
+    }
 }
