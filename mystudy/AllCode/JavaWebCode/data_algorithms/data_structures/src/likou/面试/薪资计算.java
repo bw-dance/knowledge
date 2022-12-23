17f785d5f1aff88b71d5cf1fa2b7a30b88591af2
zhq <1427421650@qq.com>
2022-12-23 22:30:45 +0800

    save note

--- mystudy/AllCode/JavaWebCode/data_algorithms/data_structures/src/likou/面试/薪资计算.java
+++ mystudy/AllCode/JavaWebCode/data_algorithms/data_structures/src/likou/面试/薪资计算.java
@@ -9,13 +9,13 @@ public class 薪资计算 {
                 //month:多少薪资
                 int month = 13;
                 //公积金
-                double accumulationFund = 0.12;
+                double accumulationFund = 0.05;
                 //每月房租，燃气，电费
                 int rent = 3000;
                 //每月自己生活费
-                int livingExpenses = 3000;
+                int livingExpenses = 2000;
                 //已交的金额
-                int  hasSub =0;
+                int  hasSub =48000;
                 //工资计算
                 computedWages(i, month, accumulationFund, livingExpenses, rent, hasSub);
             }
@@ -25,12 +25,12 @@ public class 薪资计算 {
     private static double computedWages(int allSalary, int month, double accumulationFund, int livingExpenses, int rent, int hasSub) {
         int monthSalary = allSalary / month;
         double insuranceAndFoud = insuranceAndFoud(accumulationFund, monthSalary);
-        double tax = tax(allSalary - insuranceAndFoud - (monthSalary * (month - 12)));
+        double tax = tax(allSalary - insuranceAndFoud);
         double lifeAndRent = LifeAndRent(livingExpenses, rent);
-//       double back = back(allSalary, hasSub);
-      double back =0;
+//        double back = back(allSalary, hasSub);
+     double back =0;
         double last = allSalary - insuranceAndFoud - tax - lifeAndRent - back;
-        System.out.println("------------" + allSalary + "万-----------------");
+        System.out.println("------------" + allSalary + "-----------------");
         System.out.println("还钱：" + back);
         System.out.println("最后剩余：" + last);
         return last;
